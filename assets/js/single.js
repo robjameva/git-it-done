74d6e8cae1d61789db27294c96d1485e40a0aa83
Robert Evanik <rob.evanik@gmail.com>
2021-11-25 08:51:11 -0500

    Added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,8 +5,13 @@ var repoNameEl = document.querySelector('#repo-name');
 var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        document.location.replace("./index.html");
+    }
 }
 
 var getRepoIssues = function(repo) {
@@ -22,7 +27,7 @@ var getRepoIssues = function(repo) {
             })
         }
         else {
-            alert("There was a problem with your request!")
+            document.location.replace("./index.html");
         }
     })
 }
