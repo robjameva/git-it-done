4ffb8a96de0c4087c64b2ab2820d5da4dab00ec9
Robert Evanik <rob.evanik@gmail.com>
2021-11-25 08:35:00 -0500

    passed query parameter between html pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,13 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector('#repo-name');
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+}
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -62,4 +70,6 @@ var displayWarning = function(repo) {
 }
 
 
-getRepoIssues("facebook/react")
+
+
+getRepoName();
